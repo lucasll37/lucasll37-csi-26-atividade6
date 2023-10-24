b123ef749f20ac4e51b22fea21ab4c608cd11ac4
Lucas <lucas.silva1037@gmail.com>
2023-10-23 22:12:38 -0300

    Updates

--- src/redux/calculator/action-types.ts
+++
@@ -1,9 +0,0 @@
-export const calculatorActionsTypes = {
-  ADD: 'ADD',
-  SUBTRACT: 'SUBTRACT',
-  MULTIPLY: 'MULTIPLY',
-  DIVIDE: 'DIVIDE',
-  CLEAR: 'CLEAR',
-  SET_VALUE: 'SET_VALUE',
-  EQUALS: 'EQUALS'
-};
--- src/redux/root-reducer.ts
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import { calculatorReducer } from "./calculator/reducer";
-
-export const rootReducer = combineReducers({
-    calculator: calculatorReducer
-});
